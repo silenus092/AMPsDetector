4fa7b160022f4486c216ba735f433e0583bc87a0
Ubuntu <ubuntu@november-ed8ad.denbi.local>
2021-01-17 22:10:51 +0000

    update new AMP cluster

--- Unirep/UniRep/docker/run_cpu_docker.sh
+++
@@ -1,1 +0,0 @@
-docker run -it -p 8888:8888 -p 6006:6006 -v `pwd`:/notebooks  unirep-cpu:latest
--- Unirep/UniRep/docker/run_gpu_docker_DEPRECATED.sh
+++
@@ -1,1 +0,0 @@
-nvidia-docker run -it -p 8888:8888 -p 6006:6006 -v `pwd`:/notebooks  unirep-gpu:latest
